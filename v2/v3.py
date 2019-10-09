91639a40c022671d9f90a6b60e54ac5b26610e9a
Ingeniero Fernando <163189@ids.upchiapas.edu.mx>
2019-10-09 16:01:06 -0500

    Avances y pruebas

--- v2/v3.py
+++ v2/v3.py
@@ -52,73 +52,6 @@ def mostrarTablero(tablero):
 		print(row)
 	print("\n")
 
-def validarSiElValorLoTieneMiPapa(nodoHijo, valor):
-	copia = nodoHijo.parent
-	while copia != None:
-		if(copia.name == valor):
-			return True
-		copia = copia.parent
-	return False
-
-def buscarAlosAlrededores(pila, nodoPadre, tablero, x, y, profundidadMaxima, raiz):
-	print(raiz.height)
-	if(nodoPadre.depth <= profundidadMaxima and nodoPadre.name != -100):
-		# Ir hacia arriba
-		try:
-			if tablero[x-1][y] != -1 and x > 0:
-				# print(f'El nodo [{nodoPadre.name}] va hacia arriba')
-				debeAgregarHijo = validarSiElValorLoTieneMiPapa(nodoPadre, tablero[x-1][y])
-				if debeAgregarHijo == False:
-					id_ = uniqueID()
-					nuevoNodoHijo = Node(tablero[x-1][y], parent=nodoPadre, id=id_)
-					pila.append(nuevoNodoHijo)
-					buscarAlosAlrededores(pila, nuevoNodoHijo, tablero, x-1, y, profundidadMaxima, raiz)
-
-		except IndexError:
-			print("No puede ir hacia arriba")
-
-		# Ir hacia la derecha
-		try:
-			if tablero[x][y+1] != -1:
-				# print(f'El nodo [{nodoPadre.name}] va hacia la derecha')
-				debeAgregarHijo = validarSiElValorLoTieneMiPapa(nodoPadre, tablero[x][y+1])
-				if debeAgregarHijo == False:
-					id_ = uniqueID()
-					nuevoNodoHijo = Node(tablero[x][y+1], parent=nodoPadre, id=id_)
-					pila.append(nuevoNodoHijo)
-					buscarAlosAlrededores(pila, nuevoNodoHijo, tablero, x, y+1, profundidadMaxima, raiz)
-
-		except IndexError:
-			print("No se puede ir hacia la derecha")
-
-		# Ir hacia abajo
-		try:
-			if tablero[x+1][y] != -1:
-				# print(f'El nodo [{nodoPadre.name}] va hacia abajo')
-				debeAgregarHijo = validarSiElValorLoTieneMiPapa(nodoPadre, tablero[x+1][y])
-				if debeAgregarHijo == False:
-					id_ = uniqueID()
-					nuevoNodoHijo = Node(tablero[x+1][y], parent=nodoPadre, id=id_)
-					pila.append(nuevoNodoHijo)
-					buscarAlosAlrededores(pila, nuevoNodoHijo, tablero, x+1, y, profundidadMaxima, raiz)
-
-		except IndexError:
-			print("No se puede ir hacia abajo")
-
-		# Ir hacia la izquierda
-		try:
-			if tablero[x][y-1] != -1 and y > 0:
-				# print(f'El nodo [{nodoPadre.name}] va hacia la izquierda')
-				debeAgregarHijo = validarSiElValorLoTieneMiPapa(nodoPadre, tablero[x][y-1])
-				if debeAgregarHijo == False:
-					id_ = uniqueID()
-					nuevoNodoHijo = Node(tablero[x][y-1], parent=nodoPadre, id=id_)
-					pila.append(nuevoNodoHijo)
-					buscarAlosAlrededores(pila, nuevoNodoHijo, tablero, x, y-1, profundidadMaxima, raiz)
-
-		except IndexError:
-			print("No se puede ir hacia la izquierda")
-
 def arriba(tablero, x, y):
 	# Ir hacia arriba
 	try:
@@ -155,17 +88,16 @@ def izquierda(tablero, x, y):
 		pass
 	return None
 
-def showRecorridos(nodoPadre, tablero):
-	soluciones = []
-
-	DFS = PreOrderIter(nodoPadre)
-	print("\nDFS:\n")
-	
-	for nodo in DFS:
-		if nodo.name == -100:
-			soluciones.append(nodo)
-	print("\n\n=== SOLUCIONES === \n\n")
-	print(soluciones)
+def showRecorridos(nodoPadre):
+	preorder = PreOrderIter(nodoPadre)
+	camino = ""
+	for node in preorder:
+		if node.name == -100:
+			camino += f"{node.name}"
+			break
+		camino += f"{node.name} > "
+	print(camino)
+	return camino
 	
 
 def getCoords(tablero, valorBuscado):
@@ -179,7 +111,7 @@ def existeEnLaPila(pila, valor):
 			result = True
 	return result
 
-def doAlgorithm(size, obstaculos):
+def doAlgorithm(size, obstaculos):	
 	tablero, xInicio, yInicio = construirTablero(size, obstaculos)
 	expansionMaxima = ((len(tablero) * len(tablero)) - obstaculos) * 2
 
@@ -245,28 +177,32 @@ def doAlgorithm(size, obstaculos):
 			if(valorIzquierda != None):
 				uid = uniqueID()
 				hijo = Node(valorIzquierda, parent=nodoHijoEvaluando, id=uid)
-			
-			# if len(nodoHijoEvaluando.children) == 0:
-			# No hay hijos y por consiguiente tengo que agarrar al hermano :v
 			nodoHijoEvaluando = nodoHijoEvaluando.children[0]
 		else:
 			try:
 				nodoHijoEvaluando = rightsibling(nodoHijoEvaluando)
 			except Exception:
 				nodoHijoEvaluando = rightsibling(nodoHijoEvaluando.parent)
-	
-	UniqueDotExporter(nodoRaiz).to_picture("arbol_unique.png")
-
-	# showRecorridos(nodoRaiz, tablero)
-	imagenResultante = cv2.imread("arbol_unique.png", cv2.IMREAD_GRAYSCALE)
-	cv2.imshow('Arbol resultante', imagenResultante)
-	cv2.waitKey(0)
-	cv2.destroyAllWindows()
+	return nodoRaiz
 
 def evaluarEntradas(size, obstaculos, txtSize, txtObst):
 	txtSize.config(state=DISABLED)
 	txtObst.config(state=DISABLED)
-	doAlgorithm(size, obstaculos) 
+
+	nodoRaiz = None
+	while(True):
+		nodoRaiz = doAlgorithm(size, obstaculos)
+		if(showRecorridos(nodoRaiz).find("-100") == -1):
+			break
+	
+	UniqueDotExporter(nodoRaiz).to_picture("arbol_unique.png")
+	showRecorridos(nodoRaiz)
+	# showRecorridos(nodoRaiz, tablero)
+	imagenResultante = cv2.imread("arbol_unique.png", cv2.IMREAD_GRAYSCALE)
+	cv2.imshow('Arbol resultante', imagenResultante)
+	cv2.waitKey()
+	cv2.destroyAllWindows()
+
 	txtSize.config(state=NORMAL)
 	txtObst.config(state=NORMAL)
 	messagebox.showinfo(message="Algoritmo terminado", title="Información")
@@ -291,6 +227,6 @@ if __name__ == "__main__":
 	
 
 	# btnStart = Button(window, text="Let's go!", fg="black", command=lambda: evaluarEntradas(int(txtCuadSize.get()), int(txtObstAmmount.get()), txtCuadSize, txtObstAmmount))
-	btnStart = Button(window, text="Let's go!", fg="black", command=lambda: evaluarEntradas(5, 1, txtCuadSize, txtObstAmmount))
+	btnStart = Button(window, text="Let's go!", fg="black", command=lambda: evaluarEntradas(3, 1, txtCuadSize, txtObstAmmount))
 	btnStart.grid(column=0, row=2)
 	window.mainloop()
